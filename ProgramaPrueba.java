787be0cabed30222732712a72861f187b32e228b
YassinAmine <ya00@iesemilidarder.com>
2024-01-25 20:04:38 +0100

    Primer commit

---
+++ ProgramaPrueba.java
@@ -0,0 +1,16 @@
+package ACT4_2;
+
+ /**
+  * Crea una classe que inicialitzi una matriu quadrada, pe 10x10:
+  *     Inicialitza-la de forma que cada casella tengui un valor aleatori.
+  */
+public class ACT4_2_01 {
+
+    public static void main(String[] args) {
+        final int MIDA=10;
+        int[][] matriu = UtilitatsMatrius.generaMatriu(MIDA, 10, 100);
+
+        System.out.println();
+        UtilitatsMatrius.mostrarMatriu(matriu);
+    }
+}
